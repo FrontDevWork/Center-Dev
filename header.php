bc5fd395a703880c5c24fb8f1b9d1d09130aa80f
Gatojo <gatojogu1@gmail.com>
2020-03-17 17:40:52 -0300

    formulario

---
+++ header.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <title>FrontDev</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="plugins/fontawesome-free/css/all.css" rel="stylesheet">
+    <link href="plugins/fontawesome-free/css/brands.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg nav-principal">
+        <a class="navbar-brand" href="index.php">
+            <div class="logo">
+                <img src="img/logo-branco.png" alt="Logo">
+                <b>FrontDev</b>
+            </div>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon">
+                <i class="fas fa-bars"></i>
+            </span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav barra-navegacao">
+                <li class="nav-item item-navegacao">
+                    <a class="nav-link" href="index.php"> Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item item-navegacao">
+                    <a class="nav-link" href="empresa.php">Portfólio</a>
+                </li>
+                <li class="nav-item item-navegacao">
+                    <a class="nav-link" href="lista-imoveis.php">Sobre Nós</a>
+                </li>
+                <li class="nav-item item-navegacao">
+                    <a class="nav-link" href="">Contato</a>
+                </li>
+            </ul>
+            </a>
+        </div>
+    </nav>
